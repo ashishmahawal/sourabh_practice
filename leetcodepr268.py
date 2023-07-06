1245a71bcde4381ec84e5a1a5eabbb48f5ad9978
ashishmahawal <amahawal@cs.iitr.ac.in>
2023-07-06 15:21:42 +0530

    now this is a function of missing number

--- leetcodepr268.py
+++ leetcodepr268.py
@@ -1,6 +1,15 @@
-nums=[3,0,1]
-actual =list(range(0,len(nums)+1))
-res = set(actual)-set(nums)
-array=list(res)
-missing_number =array[0]
-print(missing_number)
+#nums=[3,0,1]
+#actual =list(range(0,len(nums)+1))
+#res = set(actual)-set(nums)
+#array=list(res)
+#missing_number =array[0]
+#print(missing_number)
+def missing_number(nums):
+    actual =list(range(0,len(nums)+1))
+    res = set(actual)-set(nums)
+    array=list(res)
+    missing_number =array[0]
+    return missing_number
+nums = [1,0,3]
+result = missing_number(nums)
+print(result)
