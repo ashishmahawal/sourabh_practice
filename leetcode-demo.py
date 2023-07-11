fe992d93a51eb683ac74d9fd6ec19c67d01a0400
ashishmahawal <amahawal@cs.iitr.ac.in>
2023-07-11 13:32:21 +0530

    comprehensionprogram

--- leetcode-demo.py
+++ leetcode-demo.py
@@ -1,2 +1,11 @@
-def sol():
-    pass
+my_string = "hello world"
+my_list =[]
+for item in my_string:
+    my_list.append(item)
+print(my_list)    
+#list comprehension
+my_string = "hello world"
+my_list = [item for item in my_string]
+print(my_list)
+my_list = [item**2 for item in range(0,12)]
+print(my_list)
