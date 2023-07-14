9a0a024db2eb3081d46fa5c43dc246e93a3bec0d
ashishmahawal <amahawal@cs.iitr.ac.in>
2023-07-14 15:20:17 +0530

    new update in code

--- leetcode2529.py
+++ leetcode2529.py
@@ -1,16 +1,30 @@
-#maximum count of positive integer and negative integer
-numlist = [-1,-2,-3,1,2,3,4]
-respositive = 0
-resnegative = 0 
+# #maximum count of positive integer and negative integer
+# numlist = [-1,-2,-3,1,2,3,4]
+# respositive = 0
+# resnegative = 0 
 
-for num in numlist:
-    if num>0:
-        respositive+=1
-    elif num<0:
-        resnegative+=1
-    elif resnegative>respositive:
-        print(resnegative)
+# for num in numlist:
+#     if num>0:
+#         respositive+=1
+#     elif num<0:
+#         resnegative+=1
+
+# if respositive>resnegative:
+#     print(respositive)
+# else:
+#     print(resnegative)    
+
+def max_count(num_list):
+    respositive = 0
+    resnegative = 0     
+    for num in num_list:
+     if num>0:
+         respositive+=1
+     elif num<0:
+         resnegative+=1
+
+    if respositive>resnegative:
+       return respositive
     else:
-        respositive> resnegative
-        print(respositive)
+       return resnegative
     
